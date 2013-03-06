f4812d31da62c91000725ba1c4258bbaf8f04512
Nathan Lowrie <nate@finelineautomation.com>
2013-03-05 20:22:57 -0500

    Add latest products to home controller.
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,21 +1,22 @@
 Spree::HomeController.class_eval do
-  
+
   before_filter :get_homepage
-  
+
   def index
     @searcher = Spree::Config.searcher_class.new(params)
     @products = @searcher.retrieve_products
-    render :template => "spree/pages/home"      
+    @latest_products = Spree::Product.order("available_on DESC").limit(16)
+    render :template => "spree/pages/home"
   end
-  
+
   private
-  
+
     def get_homepage
       @page = Spree::Page.find_by_path("/")
     end
-    
+
     def accurate_title
       @page.meta_title unless @page.nil?
     end
-  
+
 end
